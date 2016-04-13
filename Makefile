4051c12b0e9fca0c3825d575a6370b44138f648e
Alejandro Gallo <gallo@pccq05.fkf.mpg.de>
2016-04-13 19:10:38 +0200

    Update to angular

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
+.PHONY: build
+build:
+	make -C tools build
 chgcar.list:
 	ls --color=never CHG*  | tr " " "\n" | grep CHG --color=never  > chgcar.list 
--- js/nv.js
+++ js/nv.js
@@ -1,129 +1,150 @@
-var chgcarFile="chgcar.list";
-var chgcarSelectId="chgcar-list";
-var CHGCARS=[];
+angular.module("viewer", ["ui.bootstrap"])
+.controller("ViewerController", ["$scope", "$http", function($scope, $http){
 
-var CONTCAR_TYPE="vasp";
-var CONTCAR="CONTCAR";
-//var CHGCAR="CHG.cube";
+  $scope.volumetricFilesPath="chgcar.list";
 
-var MAIN_VIEWER;
+  $scope.CHGCARS=[];
+  $scope.MODELS=[];
 
-function init() {
-  console.log("Initialising...");
-  getChgcarNames();
-  MAIN_VIEWER = $3Dmol.viewers.viewer;
-  MAIN_VIEWER.setBackgroundColor(0xffffff);
-}
 
-function addChgcarToList() {
-  var container = document.getElementById(chgcarSelectId);
-  var inputs = container.getElementsByTagName("input");
+  $scope.MAIN_VIEWER=undefined;
 
-  CHGCARS=[];
-  for (var i = 0; i<inputs.length; i++){
-    var inputElement = inputs[i];
-    if (inputElement.checked) {
-      CHGCARS.push(inputElement.value);
+  $scope.init = function() {
+    console.log("Initialising...");
+    $scope.getChgcarNames();
+    //MAIN_VIEWER = $3Dmol.viewers.viewer;
+    $scope.addModelObject("CONTCAR", true); // Maybe erase in the future 
+    $scope.MAIN_VIEWER=$3Dmol.createViewer("viewer");
+    $scope.MAIN_VIEWER.setBackgroundColor(0xffffff);
+  }
+
+  $scope.addModelObject = function (name, value) {
+    var model = {};
+    var format = name.match(/\..*/);
+    if (format) {
+      model.format = format[0].replace(".","");
+    }
+    else {
+      //this would suit me
+      model.format="vasp";
+    }
+    model.name = name;
+    model.value = value? true: false;
+    $scope.MODELS.push(model);
+  }
+
+  $scope.addChgcarObject = function(name) {
+    var chgcarObject={};
+    var format = name.match(/\..*/);
+    if (format) {
+      chgcarObject.format = format[0].replace(".","");
+    }
+    else {
+      chgcarObject.format="unknown";
+    }
+    chgcarObject.name = name;
+    chgcarObject.data = false;
+    chgcarObject.value = false;
+    chgcarObject.isovalue = 0.01;
+    chgcarObject.opacity = 0.95;
+    chgcarObject.alpha = 0.5;
+    chgcarObject.smoothness = 1;
+    chgcarObject.voxel = false;
+    chgcarObject.color = "blue";
+    $scope.CHGCARS.push(chgcarObject);
+  }
+
+  $scope.getChgcarNames = function() {
+    console.log("Reading chgcars from  "+$scope.volumetricFilesPath);
+    $http.get($scope.volumetricFilesPath).then(function(response){
+      var data = response.data;
+      console.log(data);
+      data = data.split(/[\n\r]/);
+      data.forEach(function(name){
+        if (name) {
+          $scope.addChgcarObject(name);
+          console.log($scope.CHGCARS);
+        }
+      });
+    });
+  }
+
+
+  $scope.clear = function() {
+    console.log("Clearing..");
+    $scope.MAIN_VIEWER.clear();
+  }
+
+
+
+  $scope.render = function() {
+    $scope.clear();
+    $scope.renderModels();
+    $scope.renderChgcar();
+    $scope.MAIN_VIEWER.setSlab(-20,20);
+    $scope.MAIN_VIEWER.render();
+    //$scope.MAIN_VIEWER.zoomTo();
+  }
+
+  $scope.renderIso = function() {
+    $scope.MAIN_VIEWER.removeAllShapes();
+    $scope.MAIN_VIEWER.removeAllSurfaces();
+    $scope.renderChgcar();
+  }
+
+  $scope.renderVolumetricData = function (chgcarObject) {
+    var isovalue   = chgcarObject.isovalue;
+    var opacity    = chgcarObject.opacity;
+    var alpha      = chgcarObject.alpha;
+    var smoothness = chgcarObject.smoothness;
+    var voxel      = chgcarObject.voxel;
+    var format     = chgcarObject.format;
+    var color     = chgcarObject.color;
+    var volumetric_path = chgcarObject.name;
+    if (chgcarObject.data) {
+      $scope.MAIN_VIEWER.addIsosurface(chgcarObject.data , {voxel:voxel , isoval: isovalue  , color: color, opacity:opacity , smoothness:smoothness , alpha: alpha});
+      $scope.MAIN_VIEWER.render();
+    } else {
+      console.log("Loading volumetric_data from "+volumetric_path);
+      $http.get(volumetric_path).then(function (response) {
+        console.log("Volumetric data received");
+        var data       = response.data;
+        var voldata    = new $3Dmol.VolumeData(data, format);
+        chgcarObject.data = voldata;
+        $scope.MAIN_VIEWER.addIsosurface(voldata , {voxel:voxel , isoval: isovalue  , color: color, opacity:opacity , smoothness:smoothness , alpha: alpha});
+        $scope.MAIN_VIEWER.render();
+      });
     }
   }
-}
 
-function getChgcarNames() {
-  console.log("Reading file "+chgcarFile);
-
-  var container = document.getElementById(chgcarSelectId);
-
-  $.get(chgcarFile, function(data){
-    data = data.split(/[\n\r]/);
-    data.forEach(function(name){
-      var li = document.createElement("li");
-      var label = document.createElement("label");
-      label.innerHTML = name;
-      var checkbox = document.createElement("input");
-      checkbox.type="checkbox";
-      checkbox.value=name;
-      checkbox.addEventListener("change", addChgcarToList);
-
-      if (name) {
-        container.appendChild(li);
-        li.appendChild(label);
-        li.appendChild(checkbox);
+  $scope.renderChgcar = function() {
+    console.log("Rendering Chgcar");
+    $scope.CHGCARS.forEach(function(chgcarObject, index){
+      if (chgcarObject.value) {
+        $scope.renderVolumetricData(chgcarObject);
       }
-
     });
-  },"text");
-}
 
+  }
 
-function clear() {
-  console.log("Clearing..");
-  MAIN_VIEWER.clear();
-}
+  $scope.renderModel = function (model) {
+    var modelPath=model.name;
+    var format=model.format;
+    $http.get(modelPath).then(function(response){
+      console.log("Structural data received");
+      var data= response.data;
+      var model = $scope.MAIN_VIEWER.addModel(data, format);
+      model.setStyle({}, {sphere:{scale: 0.2}, stick:{radius:0.1}});
+      $scope.MAIN_VIEWER.render();
+    });
+  }
+  $scope.renderModels = function() {
+    $scope.MODELS.forEach(function(model){
+      $scope.renderModel(model); 
+    });
+  }
 
+  $scope.init();
 
-function getBool(inputId) {
-  //Get boolean values from input
-  var input = document.getElementById(inputId);
-  var val = input.checked;
-  console.log("Drawing for "+inputId+" = "+val);
-  return val;
-}
-function getF(inputId) {
-  //Get float values from input
-  var input = document.getElementById(inputId);
-  var val = parseFloat(input.value);
-  console.log("Drawing for "+inputId+" = "+val.toString());
-  return val;
-}
-
-function render() {
-  clear();
-  renderModel();
-  renderChgcar();
-  MAIN_VIEWER.setSlab(-20,20);
-  MAIN_VIEWER.render();
-  //MAIN_VIEWER.zoomTo();
-}
-
-function renderIso() {
-  MAIN_VIEWER.removeAllShapes();
-  MAIN_VIEWER.removeAllSurfaces();
-  renderChgcar();
-}
-
-function renderChgcar() {
-  console.log("Rendering Chgcar");
-
-  //var volumetric_path=CHGCAR;
-
-  CHGCARS.forEach(function(volumetric_path, index){
-    if (volumetric_path) {
-      console.log("Loading volumetric_data from "+volumetric_path);
-      $.get(volumetric_path, function (data) {
-        console.log("Volumetric data received");
-        var isovalue=getF("isovalue");
-        var opacity=getF("opacity");
-        var alpha=getF("alpha");
-        var smoothness=getF("smoothness");
-        var voxel=getBool("voxel");
-        var voldata = new $3Dmol.VolumeData(data, "cube");
-        MAIN_VIEWER.addIsosurface(voldata, {voxel:voxel, isoval: isovalue, color: "blue", opacity:opacity, smoothness:smoothness, alpha: alpha});
-        MAIN_VIEWER.addIsosurface(voldata, {voxel:voxel, isoval: -isovalue, color: "red", opacity:opacity, smoothness:smoothness, alpha: alpha});
-        MAIN_VIEWER.render();
-      }, "text");
-    }
-  });
-
-}
-
-function renderModel() {
-  $.get(CONTCAR, function(data){
-    console.log("Structural data received");
-    var model = MAIN_VIEWER.addModel(data, CONTCAR_TYPE);
-    model.setStyle({}, {sphere:{scale: 0.2}, stick:{radius:0.1}});
-        MAIN_VIEWER.render();
-  }, "text");
-}
-
-
+}]);
 
