48e5c48ebb13de58d0ea6f0de2dcf5eaf4c4979c
ale <ale@lenovo.domain>
2016-07-29 22:35:30 +0200

    added css support for the build script and added bootstrap css

--- tools/build.sh
+++ tools/build.sh
@@ -18,15 +18,34 @@ mkdir -p ${BUILD_DIR}
 
 header "Examining $VIEWER..."
 js_files=$(cat $VIEWER | grep "<script" | grep src | sed -e "s/.*src=\"\(\S*\)\".*/\1/")
+css_files=$(cat $VIEWER | grep "<link" | grep href | sed -e "s/.*href=\"\(\S*\)\".*/\1/")
 
 
 header "Creating new viewer in $BUILD_VIEWER "
 cat $VIEWER | sed -e "/script.*src.*/d" -e "/<\/body/d" -e "/<\/html/d" > $BUILD_VIEWER
 
-
 arrow "Going into the directory of the viewer ${VIEWER_DIR}"
 cd ${VIEWER_DIR}
 
+header "CSS files"
+for fileName in $css_files; do
+  arrow "Parsing \033[0;31m\033[0m $fileName"
+
+  if [ ! -f $fileName ]; then
+    error "File $fileName not found"
+    continue
+  fi
+
+  sed -i  "\:${fileName}:{ 
+i <style type='text/css' media='screen'>
+r ${fileName}
+a </style>
+d
+}" ${BUILD_VIEWER}
+
+done
+
+header "JS files"
 for fileName in $js_files; do
 
   arrow "Parsing file $fileName"
